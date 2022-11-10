abf232be4fa232b1167cf2bfea3d6cafe2aab489
Faisal Mohammed <m.faisal244@gmail.com>
2022-11-10 00:31:40 +0000

    Search Form component complete

--- src/components/SearchForm/Index.js
+++ src/components/SearchForm/Index.js
@@ -1,59 +1,40 @@
-// const SearchForm = (props) => {
-// 	return (
-// 		<div className="search">
-// 			<form onSubmit={props.onSubmit}>
-// 				<div className="row h-100 search-form">
-// 					<div className="col-sm-12 col-md-10 p-2">
-// 						<input
-// 							type="text"
-// 							className="form-control"
-// 							id="search-city"
-// 							placeholder={props.placeholder}
-// 							onChange={props.onChange}
-// 							value={props.value}
-// 						/>
-// 					</div>
-// 					<div className="col-sm-12 col-md-2 p-2">
-// 						<button
-// 							type="submit"
-// 							className="btn btn-primary"
-// 						>
-// 							Search
-// 						</button>
-// 					</div>
-// 				</div>
-// 			</form>
-// 		</div>
-// 	);
-// };
-
-// export default SearchForm;
-
-import { useState, useEffect, useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import classNames from "classnames";
 import axios from "axios";
-import { AppContext } from "../../App";
+
+import "semantic-ui-css/semantic.min.css";
 import "../../styles.css";
+import { AppContext } from "../../App";
 
 const API_KEY = "393609ac7b2e5f25ccdd00e626ee13dd";
 
 export const SearchForm = () => {
-	const { searchTerm, setSearchTerm, cities, setCities } =
+	const { searchTerm, setSearchTerm, setCities, cities, setWeatherData } =
 		useContext(AppContext);
 	const [isLoading, setIsLoading] = useState(false);
-	// const [searchTerm, setSearchTerm] = useState("");
 	const [inputValue, setInputValue] = useState("");
 
 	useEffect(() => {
 		if (searchTerm) {
-			setIsLoading(true);
 			(async () => {
+				setIsLoading(true);
+
 				const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=${API_KEY}`;
-				// console.log("make API request for ", url);
 
 				const { data } = await axios.get(url);
 				console.log(data);
-				setCities([...cities, searchTerm]);
+				// setWeatherData(data);
+
+				const citiesFromLS = JSON.parse(localStorage.getItem("cities")) || [];
+
+				if (!citiesFromLS.includes(searchTerm)) {
+					citiesFromLS.push(searchTerm);
+					localStorage.setItem("cities", JSON.stringify(citiesFromLS));
+					setCities([...cities, searchTerm]);
+				}
+
+				setSearchTerm("");
+				setInputValue("");
 				setIsLoading(false);
 			})();
 		}
@@ -80,12 +61,10 @@ export const SearchForm = () => {
 						setInputValue(event.currentTarget.value);
 					}}
 					type="text"
-					placeholder="Enter City Name"
+					placeholder="Enter city name"
 				/>
 				<i className="search icon"></i>
 			</div>
 		</form>
 	);
 };
-
-export default SearchForm;
